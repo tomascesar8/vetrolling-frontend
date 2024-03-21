5b01b5da1868b8833b2a99df2297599aaa9cd541
Tomás César <tomascesar1993@gmail.com>
2024-03-21 19:20:18 -0300

    deploy vercel

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,5 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
